5b05cde67d18d19a52ecbddb40132a239b23c845
gdiego07 <40152772+gdiego07@users.noreply.github.com>
2023-08-21 16:59:25 -0700

    updated the header with phone icon - updated the banner section with text and a button

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def index():
     return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
